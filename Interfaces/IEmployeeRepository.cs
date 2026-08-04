326dd214e6954bf51cb0f28d0fc1cda2b7aada2c
Muhammad Khatri <muhammadkhatri77@gmail.com>
2026-08-04 21:50:40 +0500

    some front changes

--- Interfaces/IEmployeeRepository.cs
+++ Interfaces/IEmployeeRepository.cs
@@ -4,6 +4,7 @@ namespace HR_system.Interfaces
 {
     public interface IEmployeeRepository
     {
+        // searchTerm is optional, and use for the module number 4 
         Task<(IEnumerable<Employee> Employees, int TotalCount)> GetAllAsync(string? searchTerm, int pageNumber, int pageSize);
         Task<Employee?> GetByIdAsync(int id);
         Task AddAsync(Employee employee);
--- obj/Debug/net8.0/HR_system.AssemblyInfo.cs
+++ obj/Debug/net8.0/HR_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e2ac57488169f22903e5d4207ac74853e7ca05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a188a86e675bc5005e3f0682c785646db38569")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
