f44fbbbb9bf85a653ed98c7870cbcd6c6db4aebd
Muhammad Khatri <muhammadkhatri77@gmail.com>
2026-08-26 21:33:41 +0500

    Employee index update

--- obj/Debug/net8.0/HR_system.AssemblyInfo.cs
+++ obj/Debug/net8.0/HR_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33df3fb51cb4a51e1cd16b5b8747ba85b8877765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc74cbf37d1124dbd0dd908e0ecdc570f189d85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
