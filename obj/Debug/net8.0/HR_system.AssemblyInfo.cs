075526d0540c070005775116238520982d73d4e4
Muhammad Khatri <muhammadkhatri77@gmail.com>
2026-09-01 08:31:23 +0500

    Dashboard is shifted

--- obj/Debug/net8.0/HR_system.AssemblyInfo.cs
+++ obj/Debug/net8.0/HR_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e66e5a0dd6fb19c0f7d823fd3b6b2b1dbefe07c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd736cff137c63b7b4d89c73c9c0a3dd9ab6634")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
